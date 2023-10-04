69204f27358205915fd3fa63c86922e01f206680
Alexandrbig1 <alexsmagin1@gmail.com>
2023-10-04 07:07:08 -0500

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,56 +25,45 @@ export const App = () => {
   const percent = Math.floor((good / total) * 100);
 
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-        <span>Good: {good}</span>
-        <span>Neutral: {neutral}</span>
-        <span>Bad: {bad}</span>
-        <span>Total: {total}</span>
-        <span>Positive feedback: {total === 0 ? '' : percent}%</span>
-      </div>
-      <div style={{ display: 'block', marginBottom: '20px' }}>
-        <button type="button" onClick={handlerOnClickGood}>
-          Good
-        </button>
-        <button type="button" onClick={handlerOnClickNeutral}>
-          Neutral
-        </button>
-        <button type="button" onClick={handlerOnClickBad}>
-          Bad
-        </button>
+    <div className="container">
+      <div className="wrapper">
+        <div className="btn-wrapper">
+          <button
+            className="btn btn-good"
+            type="button"
+            onClick={handlerOnClickGood}
+          >
+            Good
+          </button>
+          <button
+            className="btn btn-neutral"
+            type="button"
+            onClick={handlerOnClickNeutral}
+          >
+            Neutral
+          </button>
+          <button
+            className="btn btn-bad"
+            type="button"
+            onClick={handlerOnClickBad}
+          >
+            Bad
+          </button>
+        </div>
+        <p className="text-wrapper">
+          <span>Good: {good}</span>
+          <span>Neutral: {neutral}</span>
+          <span>Bad: {bad}</span>
+          <span>Total: {total}</span>
+          <span>
+            {total === 0 ? (
+              <span>Positive feedback: </span>
+            ) : (
+              <span>Positive feedback: {percent}%</span>
+            )}
+          </span>
+        </p>
       </div>
     </div>
   );
 };
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101',
-//       }}
-//     >
-//       <Statistics
-//       // good={1}
-//       // neutral={1}
-//       // bad={1}
-//       // total={3}
-//       // positivePercentage={2}
-//       />
-//     </div>
-//   );
-// };
